71ea7f1cab7fe9dc08343bc79eef13735ada49f8
Matthew  Lanci <mattlanci@148-100-157-183.FoxNet.marist.edu>
2018-01-29 17:58:19 -0500

    solution to curiosity rover exercise

---
+++ Rover.py
@@ -0,0 +1,10 @@
+def main():
+    print ("Time it takes for photo to reach NASA from MArs Rover")
+    distance = 34000000
+    speed = 186000
+    print ("time of travel is", distance/speed, "in seconds")
+
+main()
+
+
+
