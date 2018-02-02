555789d5da7c9713bfbb28349f406904969d1229
Matthew  Lanci <mattlanci@148-100-157-183.FoxNet.marist.edu>
2018-02-02 16:04:21 -0500

    solution to Mad Libs Exercise

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+noun= input("Enter your noun: ")
+def madlib():
+    noun = input("Enter a Noun")
+    verb = input("Enter a verb")
+    adjective = input("Enter an adjective")
+    place = input("Enter a place")
+
+    print ("The", adjective, noun, "was", verb, "at", place)
