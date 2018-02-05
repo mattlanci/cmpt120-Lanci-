08ffb181e1b5a157d71d4c54371ff77c62293b7f
Matthew  Lanci <mattlanci@148-100-157-183.FoxNet.marist.edu>
2018-02-05 18:35:05 -0500

    wrote an interactive fibonacci calculator

---
+++ fibocacci.py
@@ -0,0 +1,19 @@
+def main():
+    print("This program calculates the nth Fibocacci value.")
+    print()
+
+    n = int(input("Enter the Value of n: ")
+    curr, prev = curr+prev, curr
+
+    print()
+    print("The nth Fibocacci number is", curr)
+
+main()
+    
+
+
+    
+    
+
+
+    
