4ba9a521b856ad9bb1d83acb6df1acc9ef4b88f0
Juan Arias <juan.arias@marist.edu>
2018-02-11 21:31:32 -0500

    Feedback

--- fibocacci.py
+++ fibocacci.py
@@ -2,8 +2,10 @@ def main():
     print("This program calculates the nth Fibocacci value.")
     print()
 
-    n = int(input("Enter the Value of n: ")
-    curr, prev = curr+prev, curr
+    n = int(input("Enter the Value of n: ")) # JA
+    curr, prev = 1, 1 # JA: You have to initialize both
+    for i in range(n - 2): # JA
+        curr, prev = curr+prev, curr
 
     print()
     print("The nth Fibocacci number is", curr)
