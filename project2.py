cb0704ac4b7612cde6db74d207e44d6ec8acac89
Matthew  Lanci <mattlanci@Matthews-MBP.fios-router.home>
2018-03-11 14:59:29 -0400

    Project 2

---
+++ project2.py
@@ -0,0 +1,36 @@
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def change_sign(num1):
+    result = (num1 * -1)
+    return result
+
+def square(num1):
+    result = num1 * num1
+    return result
+
+def over_x(num1):
+    x = num1
+    result = 1 / x
+    return float (result)
+
+def percent(num1):
+    result = num1 / 100
+    return float (result)
+
+def square_root(num1):
+    result = sqrt(num1)
+    return float (result)
